e70a95224771545b51bed994b05b63cef8c8b9f3
GAURAV SHARMA <heygaurav54@gmail.com>
2026-05-22 00:34:52 +0530

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,14 @@
+integer_type=1
+float_type=44.3
+bool_type=False
+string_type="Hi"
+list_type=[22,"Hi",False,44.3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
